be3036e0ee68fbe0160035b7494de8848540ac80
adamsibnhajia <117345085+adamsibnhajia@users.noreply.github.com>
2023-11-09 21:27:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const showbtn = document.querySelector(".nav-btn");
+    const closebtn = document.querySelector(".close-btn");
+    const sidebar = document.querySelector(".sidebar");
+  
+    showbtn.addEventListener("click", () => {
+      sidebar.classList.add("showcontent");
+    });
+  
+    closebtn.addEventListener("click", () => {
+      sidebar.classList.remove("showcontent");
+    });
+  });
+  
